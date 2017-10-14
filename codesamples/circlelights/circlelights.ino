a4846b2fadbb8a8c58207bc42ea0425a2fcf8121
JENNIFER WADELLA <jennifer.wadella@gmail.com>
2017-10-14 00:16:04 -0500

    updating slides for devup conf 2017

--- codesamples/circlelights/circlelights.ino
+++ codesamples/circlelights/circlelights.ino
@@ -8,16 +8,16 @@ Timer timefor;
 
 void loop() {
   // put your main code here, to run repeatedly:
-    light.turn_on_single(SW, );
+    light.turn_on_single(SW, RED);
     timefor.pause(150);
     light.turn_off_single(SW);
-    light.turn_on_single(NW, colors[i]);
+    light.turn_on_single(NW, RED);
     timefor.pause(150);
     light.turn_off_single(NW);
-    light.turn_on_single(NE, colors[i]);
+    light.turn_on_single(NE, RED);
     timefor.pause(150);
     light.turn_off_single(NE);
-    light.turn_on_single(SE, colors[i]);
+    light.turn_on_single(SE, RED);
     timefor.pause(150);
     light.turn_off_single(SE);
   
