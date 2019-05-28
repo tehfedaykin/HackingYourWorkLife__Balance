4a6e4d2ca324645c15c257d5045d4c73a43325a4
tehfedaykin <jennifer.wadella@gmail.com>
2019-05-28 18:31:32 -0500

    techorama updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /* global module:false */
 module.exports = function(grunt) {
-	var port = grunt.option('port') || 8000;
+	var port = grunt.option('port') || 1337;
 	var root = grunt.option('root') || '.';
 
 	if (!Array.isArray(root)) root = [root];
